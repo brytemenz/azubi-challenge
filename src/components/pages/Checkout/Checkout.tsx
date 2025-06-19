6e6c6533228005a18951b99d153642f081a6ecba
Bright Mensah <desarrollo1.innocv@testainmo.com>
2025-06-19 21:16:27 +0000

    updated checkout page

--- src/components/pages/Checkout/Checkout.tsx
+++ src/components/pages/Checkout/Checkout.tsx
@@ -47,7 +47,7 @@ export const checkoutAction = async function ({ request }: { request: Request })
   }
 
   try {
-    const res = await fetch('https://azubi-assesment-w8cm.vercel.app/create-order', {
+    const res = await fetch('https://azubi-challenge.vercel.app/create-order', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
